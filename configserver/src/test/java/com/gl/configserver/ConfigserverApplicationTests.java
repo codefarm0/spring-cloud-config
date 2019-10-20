3af983dce7f9e6f8e2582c6553c7a47c7a3d3cee
Green Learner <greenlearner01@users.noreply.github.com>
2019-10-20 17:46:49 +0530

    updated

--- configserver/src/test/java/com/gl/configserver/ConfigserverApplicationTests.java
+++ configserver/src/test/java/com/gl/configserver/ConfigserverApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gl.configserver;
+package com.gl.configServer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigserverApplicationTests {
+public class ConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
